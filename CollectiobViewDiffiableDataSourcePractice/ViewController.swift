bb546a8b2241da112818201bda93476699d78ca5
Vivian Phung <9276753+VPhung24@users.noreply.github.com>
2023-02-19 11:45:16 -0500

    Initial Commit

---
+++ CollectiobViewDiffiableDataSourcePractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectiobViewDiffiableDataSourcePractice
+//
+//  Created by Vivian Phung on 2/19/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
